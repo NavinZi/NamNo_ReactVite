1579ba1f902611ff78223cdcdaa5c29b355a2f83
Navin <navin@192.168.1.45>
2022-09-10 22:33:23 +0700

    test commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -85,7 +85,7 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-        <h1>Home Page</h1>
+        <h1>Home Pages</h1>
       </div>
     </div>
   );
